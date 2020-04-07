feddd4a1968aa1da646f41d27f79d36a7dda7bc9
JosiahOnline <josiahriverflow@hotmail.com>
2020-04-07 21:47:41 +0800

    Updates

---
+++ precache-manifest.21745867e1e0b8b401a02253a558de55.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bdf1af6a37e1e85f47625314148ec8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "788df24dbdf7658d63d5",
+    "url": "/static/css/main.9419a4c2.chunk.css"
+  },
+  {
+    "revision": "3873f5f9e812b8b817d4",
+    "url": "/static/js/2.22917e96.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.22917e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788df24dbdf7658d63d5",
+    "url": "/static/js/main.8182827b.chunk.js"
+  },
+  {
+    "revision": "5bcca5387be8f948934f",
+    "url": "/static/js/runtime-main.5179d178.js"
+  },
+  {
+    "revision": "60197c736ea73f834fdb350f302f4565",
+    "url": "/static/media/Supreme Logo.60197c73.png"
+  },
+  {
+    "revision": "c8eff22e2a03526f56180dcd71d5f854",
+    "url": "/static/media/location.c8eff22e.png"
+  },
+  {
+    "revision": "caced1e9ddb1deb426ef5d409edc1fe5",
+    "url": "/static/media/pic1.caced1e9.jpg"
+  },
+  {
+    "revision": "7670bd992d55e25ae15ad0dc2663c81e",
+    "url": "/static/media/pic2.7670bd99.jpg"
+  },
+  {
+    "revision": "925332922b87cce13c998a85f5719043",
+    "url": "/static/media/pic3.92533292.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a6cd57f4bff01d9f3860c3ab15e5037.js"
+  "/precache-manifest.21745867e1e0b8b401a02253a558de55.js"
 );
 
 self.addEventListener('message', (event) => {
